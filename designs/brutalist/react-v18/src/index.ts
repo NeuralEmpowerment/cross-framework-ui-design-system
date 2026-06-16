a44f0e677fdf96f4c8d9c1a87d161b6dafbdc92a
NeuralEmpowerment <129192050+NeuralEmpowerment@users.noreply.github.com>
2026-06-16 13:06:10 -0700

    feat(brutalist-react-v18): add contract-adapter and barrel export

---
+++ designs/brutalist/react-v18/src/index.ts
@@ -0,0 +1,11 @@
+export { Badge } from "./components/Badge.js";
+export { Button } from "./components/Button.js";
+export { Toggle } from "./components/Toggle.js";
+export { reactV18ContractAdapter } from "./contract-adapter.js";
+export type {
+  ReactV18AdapterProps,
+  ReactV18ContractConformance
+} from "./contract-adapter.js";
+export type { BadgeProps } from "./components/Badge.js";
+export type { ButtonProps } from "./components/Button.js";
+export type { ToggleProps } from "./components/Toggle.js";
